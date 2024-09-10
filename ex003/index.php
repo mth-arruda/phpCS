554bef1bf06e0e4edfb1b76d1c924c523ecf44ed
Matheus <mthRamos.silva@gmail.com>
2024-09-09 22:35:27 -0400

    phpCS

---
+++ ex003/index.php
@@ -0,0 +1,24 @@
+<?php 
+//Variaveis e constantes
+//v-Posso mudar a qualquer momento no meu codigo
+//$-Simbolo, apenas para variaveis
+//
+//
+//c- nao precisa de '$' e voce da seu nome e atribui algum valor
+//
+//
+$nome = "Matheus";
+$sobrenome = "Arruda";
+const Pais = "Brasil";
+//
+//
+//$sobrenome = "Silva";
+//variavel so guarda um valor, e pode ser subsituida no meio do cod
+//
+//
+//A const que ja foi definida, nao pode mudar no meio do cod
+//
+//
+echo "Muito Prazer, $nome $sobrenome! Voce mora no ". Pais ;
+
+?>
